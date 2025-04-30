dc37a9f42ca6e5bb992f250eb20e0f37266e6329
R2B <robeats.hale@gmail.com>
2025-05-01 00:30:44 +0200

    fix: safer table init for mutexes

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rha-le <rha-le@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 14:28:32 by rha-le            #+#    #+#             */
-/*   Updated: 2025/04/30 20:04:20 by rha-le           ###   ########.fr       */
+/*   Updated: 2025/05/01 00:29:25 by rha-le           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,14 @@
 #include <stdio.h>
 int	main(int argc, char *argv[])
 {
-	t_table	table;
-	t_philo	*philo;
+	static t_table	table = {0};
+	t_philo			*philo;
 
 	philo = NULL;
-	memset(&table, 0, sizeof(table));
 	if (init(&philo, &table, argc, argv))
 		return (EXIT_FAILURE);
 	if (simulation(philo, &table))
 		return (EXIT_FAILURE);
-	printf("SUCCESS!\n");
+	printf("SUCCESS!\n"); // TODO: delete later!
 	return (EXIT_SUCCESS);
 }
