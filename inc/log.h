ff59f5d3433ad504a61f3688f90a5399fd1695b2
R2B <robeats.hale@gmail.com>
2025-05-01 00:28:26 +0200

    feat: log function for philos

--- inc/log.h
+++ inc/log.h
@@ -6,12 +6,14 @@
 /*   By: rha-le <rha-le@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 14:48:42 by rha-le            #+#    #+#             */
-/*   Updated: 2025/04/30 16:49:09 by rha-le           ###   ########.fr       */
+/*   Updated: 2025/05/01 00:22:49 by rha-le           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LOG_H
 # define LOG_H
+
+# include "structs.h"
 
 # define WRONG_NUM_OF_ARG_MSG "Wrong amount of arguments! Expected: 4"
 # define INVALID_PHILO_SIZE_MSG "Invalid number of Philosophers!"
@@ -20,7 +22,9 @@
 # define INVALID_SLEEP_TIME_MSG "Invalid time_to_sleep!"
 
 # define ERR_INIT_FORKS_MSG "Failed to initialize forks!"
+# define ERR_INIT_MUTEX_MSG "Failed to initialize mutex!"
 
 void	log_msg(char *msg);
+void	log_philo(uint64_t timestamp, t_philo *philo, char *status);
 
 #endif // !LOG_H
--- src/log.c
+++ src/log.c
@@ -6,13 +6,25 @@
 /*   By: rha-le <rha-le@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 14:45:48 by rha-le            #+#    #+#             */
-/*   Updated: 2025/04/30 15:13:49 by rha-le           ###   ########.fr       */
+/*   Updated: 2025/05/01 00:27:24 by rha-le           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include <stdio.h>
+#include "structs.h"
 
 void	log_msg(char *msg)
 {
 	printf("%s\n", msg);
+}
+
+int	log_philo(uint64_t timestamp, t_philo *philo, char *status)
+{
+	if (pthread_mutex_lock(&philo->table->log_mutex))
+		return (EXIT_FAILURE);
+	printf("%lu %i %s\n", timestamp, philo->id, status);
+	if (pthread_mutex_unlock(&philo->table->log_mutex))
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
