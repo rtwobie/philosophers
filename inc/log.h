7788d969015f013b36d6792aaa029ddf3d92af3f
Robbie Nhat Anh Ha Le <rha-le@c4b8c6.42berlin.de>
2025-05-01 17:28:33 +0200

    feat: better log_function + macros for statuses

--- inc/log.h
+++ inc/log.h
@@ -6,7 +6,7 @@
 /*   By: rha-le <rha-le@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 14:48:42 by rha-le            #+#    #+#             */
-/*   Updated: 2025/05/01 00:22:49 by rha-le           ###   ########.fr       */
+/*   Updated: 2025/05/01 17:17:05 by rha-le           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,25 @@
 
 # include "structs.h"
 
+//******************* STATUS MESSAGES *******************//
+# define DIE "died"
+# define EAT "is eating"
+# define SLEEP "is sleeping"
+# define THINK "is thinking"
+# define FORK "has taken a fork"
+# define DEBUG_FORK_R "has taken the right fork"
+# define DEBUG_FORK_L "has taken the left fork"
+
+//******************* ERROR MESSAGES *******************//
 # define WRONG_NUM_OF_ARG_MSG "Wrong amount of arguments! Expected: 4"
 # define INVALID_PHILO_SIZE_MSG "Invalid number of Philosophers!"
 # define INVALID_DIE_TIME_MSG "Invalid time_to_die!"
 # define INVALID_EAT_TIME_MSG "Invalid time_to_eat!"
 # define INVALID_SLEEP_TIME_MSG "Invalid time_to_sleep!"
-
 # define ERR_INIT_FORKS_MSG "Failed to initialize forks!"
 # define ERR_INIT_MUTEX_MSG "Failed to initialize mutex!"
 
 void	log_msg(char *msg);
-void	log_philo(uint64_t timestamp, t_philo *philo, char *status);
+int		log_philo(uint64_t timestamp, t_mutex *log_mutex, unsigned int id, char *status);
 
 #endif // !LOG_H
--- src/log.c
+++ src/log.c
@@ -6,7 +6,7 @@
 /*   By: rha-le <rha-le@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 14:45:48 by rha-le            #+#    #+#             */
-/*   Updated: 2025/05/01 00:27:24 by rha-le           ###   ########.fr       */
+/*   Updated: 2025/05/01 16:09:52 by rha-le           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ void	log_msg(char *msg)
 	printf("%s\n", msg);
 }
 
-int	log_philo(uint64_t timestamp, t_philo *philo, char *status)
+int	log_philo(uint64_t timestamp, t_mutex *log_mutex, unsigned int id, char *status)
 {
-	if (pthread_mutex_lock(&philo->table->log_mutex))
+	if (pthread_mutex_lock(log_mutex))
 		return (EXIT_FAILURE);
-	printf("%lu %i %s\n", timestamp, philo->id, status);
-	if (pthread_mutex_unlock(&philo->table->log_mutex))
+	printf("%lu %i %s\n", timestamp, id, status);
+	if (pthread_mutex_unlock(log_mutex))
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
